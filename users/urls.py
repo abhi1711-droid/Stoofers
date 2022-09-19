f55aef6b5125a63b89ce9ccda7aa8ca4ade1a019
abhi1711-droid <abhisheksinha1709@gmail.com>
2022-09-20 01:00:48 +0530

    changes made :1

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout_user", views.logout_user, name="logout_user"),
-    path("signup/", views.signup, name="signup"),
+    path("", views.signup, name="signup"),
     path("details/", views.customer, name="customer"),
     path("stooferscard/", views.stooferscard, name="stooferscard"),
 ]
